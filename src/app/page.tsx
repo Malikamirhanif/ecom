2bbc418fb4bf6320ea5286c37b26d06f8152923c
Malikamirhanif <malikamir03456621723@gmail.com>
2023-08-22 18:51:39 +0500

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,19 @@ import { NextResponse } from 'next/server'
 
  async function fetchProducts()
  {
-  // let res=await fetch(`${BASE_PATH_URL}/api/products`);
-  // if(!res.ok)
-  // {
-  //   throw new Error('Failed to fetch');
-  // }
-  // return res.json();
-  return NextResponse.json({message:"hi"})
+  let res=await fetch(`${BASE_PATH_URL}/api/products`);
+  if(!res.ok)
+  {
+    throw new Error('Failed to fetch');
+  }
+  return res.json();
+  // return NextResponse.json({message:"hi"})
 }
 
 export default async function Home() {
 
-  // let response=await fetchProducts();
-  // console.log({"response":response});
+  let response=await fetchProducts();
+  console.log({"response":response});
   return (
     <main>
       {/* <Hero/>
