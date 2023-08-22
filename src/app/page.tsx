f3ce2e150a60004306d7323144a177d2a1b7a1fb
Malikamirhanif <malikamir03456621723@gmail.com>
2023-08-22 18:49:11 +0500

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,17 @@ import Hero from '@/views/Hero'
 import Promotions from '@/views/Promotions'
 import NewsLetter from '@/views/NewsLetter'
 import BASE_PATH_URL from '@/compoents/shared/Basepath'
+import { NextResponse } from 'next/server'
 
  async function fetchProducts()
  {
-  let res=await fetch(`${BASE_PATH_URL}/api/products`);
-  if(!res.ok)
-  {
-    throw new Error('Failed to fetch');
-  }
-  return res.json();
+  // let res=await fetch(`${BASE_PATH_URL}/api/products`);
+  // if(!res.ok)
+  // {
+  //   throw new Error('Failed to fetch');
+  // }
+  // return res.json();
+  return NextResponse.json({message:"hi"})
 }
 
 export default async function Home() {
