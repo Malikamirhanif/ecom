8b5d3184c8845b27ef2b817223baab0490d9d200
Malikamirhanif <malikamir03456621723@gmail.com>
2023-08-26 19:08:59 +0500

    n

--- src/views/ProductCarousel.tsx
+++ src/views/ProductCarousel.tsx
@@ -2,14 +2,14 @@ import {oneProductType} from '@/components/utils/SanityProductsTypes'
 import React, { FC } from 'react'
 import Cards from '@/views/Cards'
 const ProductCarousel:FC<{res:Array<oneProductType>} > = ({res}) => {
- console.log(res);
+//  console.log(res);
 // console.log("Length is "+res.length)
 
   return (
     <div className='flex gap-5'> 
        {
         
-        res.map((item:oneProductType,index:number)=>(
+        res?.map((item:oneProductType,index:number)=>(
           <Cards key={index} singleProduct={item}/>
         ))
        }
