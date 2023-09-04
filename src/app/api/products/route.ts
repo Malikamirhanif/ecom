aa2296e78691793a4ff3ac2a4ad13c85b1bf5f2d
Malikamirhanif <malikamir03456621723@gmail.com>
2023-09-04 19:24:16 +0500

    new

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,16 +2,16 @@ import { oneProductType } from "@/components/utils/SanityProductsTypes";
 import { client } from "../../../../sanity/lib/client";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request:NextRequest) {
     try {
-        //  let url=request.nextUrl.searchParams;
+         let url=request.nextUrl.searchParams;
 
         
         // return NextResponse.json({"message":"hi"})
 
        let response=await client.fetch('*[_type=="products"]');
     //    let allData=[...response]
-    //    if(url.has("st")&& url.has("ed"))
+    //    if(url.has("st")&&url.has("ed"))
     //    {
     //     if (allData[Number(url.get("st"))]){
     //         let productArray:Array<oneProductType>=allData.slice(Number(url.get("st")),Number(url.get("ed")))
