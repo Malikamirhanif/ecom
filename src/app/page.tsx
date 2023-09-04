e492e1eba3e85c0948e330b578e529c813b0b2e7
Malikamirhanif <malikamir03456621723@gmail.com>
2023-09-04 18:04:44 +0500

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import ProductCarousel from '@/views/ProductCarousel'
 import {oneProductType} from '@/components/utils/SanityProductsTypes'
 import { Root } from 'postcss'
 
- export  async function fetchProducts()
+   async function fetchProducts()
  {
   let url=`${BASE_PATH_URL}/api/products`;
   let res=await fetch(url,{cache:"no-store",});
