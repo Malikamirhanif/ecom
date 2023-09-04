652818bf26c8ca629bb7f62de01ba88dad034754
Malikamirhanif <malikamir03456621723@gmail.com>
2023-09-04 19:45:02 +0500

    new

--- src/app/pages/Products/page.tsx
+++ src/app/pages/Products/page.tsx
@@ -3,8 +3,7 @@ import { oneProductType } from '@/components/utils/SanityProductsTypes';
 import ProductCarousel from '@/views/ProductCarousel';
 
 import React from 'react'
-
-export async function fetching(){
+  async function fetching(){
     let url=`${BASE_PATH_URL}/api/products?st=0&ed=3`;
     let res=await fetch(url,{cache:"no-store",});
     // console.log(res.json())
