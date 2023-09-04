e6d876bb5e7104941e6e9ee693644886697b105f
Malikamirhanif <malikamir03456621723@gmail.com>
2023-09-04 19:21:00 +0500

    new

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,29 +2,29 @@ import { oneProductType } from "@/components/utils/SanityProductsTypes";
 import { client } from "../../../../sanity/lib/client";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request:NextRequest) {
+export async function GET() {
     try {
-         let url=request.nextUrl.searchParams;
+        //  let url=request.nextUrl.searchParams;
 
         
         // return NextResponse.json({"message":"hi"})
 
        let response=await client.fetch('*[_type=="products"]');
-       let allData=[...response]
-       if(url.has("st")&& url.has("ed"))
-       {
-        if (allData[Number(url.get("st"))]){
-            let productArray:Array<oneProductType>=allData.slice(Number(url.get("st")),Number(url.get("ed")))
-            return NextResponse.json(productArray)
-        }
-        else
-        {
-         return NextResponse.json({productArray:"Not Found"})
-        }
-       }
+    //    let allData=[...response]
+    //    if(url.has("st")&& url.has("ed"))
+    //    {
+    //     if (allData[Number(url.get("st"))]){
+    //         let productArray:Array<oneProductType>=allData.slice(Number(url.get("st")),Number(url.get("ed")))
+    //         return NextResponse.json(productArray)
+    //     }
+    //     else
+    //     {
+    //      return NextResponse.json({productArray:"Not Found"})
+    //     }
+    //    }
 
     //    console.log({allData})
-    else return NextResponse.json(allData);
+     return NextResponse.json(response);
 
     } catch (error) {
         console.log("Error",error)
