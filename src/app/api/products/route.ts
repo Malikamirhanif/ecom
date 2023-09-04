a05c1c2c10f5c700ca2f49c5feb5a4371ad8f46a
Malikamirhanif <malikamir03456621723@gmail.com>
2023-09-04 20:11:29 +0500

    new

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -5,24 +5,25 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request:NextRequest) {
     let response=await client.fetch('*[_type=="products"]');
            let url=await request.nextUrl.searchParams;
+            
+       let allData=[...response]
+       if(url.has("st")&&url.has("ed"))
+       {
+        if (allData[Number(url.get("st"))]){
+            let productArray:Array<oneProductType>=allData.slice(Number(url.get("st")),Number(url.get("ed")))
+            return NextResponse.json(productArray)
+        }
+        else
+        {
+         return NextResponse.json({productArray:"Not Found"})
+        }
     // try {
   
     //     //  console.log({url});
         
     //     // return NextResponse.json({"message":"hi"})
 
-    
-    //    let allData=[...response]
-    //    if(url.has("st")&&url.has("ed"))
-    //    {
-    //     if (allData[Number(url.get("st"))]){
-    //         let productArray:Array<oneProductType>=allData.slice(Number(url.get("st")),Number(url.get("ed")))
-    //         return NextResponse.json(productArray)
-    //     }
-    //     else
-    //     {
-    //      return NextResponse.json({productArray:"Not Found"})
-    //     }
+   
     //    }
 
     //    console.log({allData})
