ea06ef07d2b3195c88bd57414f3c1a1cd8dc97cd
Malikamirhanif <malikamir03456621723@gmail.com>
2023-09-04 18:03:39 +0500

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,31 @@
-import BASE_PATH_URL from '@/compoents/shared/Basepath'
-
-
-export  async function fetchProducts()
-{
- let url=`${BASE_PATH_URL}/api/products`;
- let res=await fetch(url,{cache:"no-store",});
- // console.log(res.json);
- if(!res.ok)
- {
-   throw new Error('Failed to fetch');
- }
- return res.json();
- // return NextResponse.json({message:"hi"})
-}
-
 import Jewellery from '@/views/Jewellery'
 import Hero from '@/views/Hero'
 import Promotions from '@/views/Promotions'
 import NewsLetter from '@/views/NewsLetter'
-
+import BASE_PATH_URL from '@/compoents/shared/Basepath'
 import { NextResponse } from 'next/server'
 import ProductCarousel from '@/views/ProductCarousel'
 import {oneProductType} from '@/components/utils/SanityProductsTypes'
 import { Root } from 'postcss'
-import { useEffect, useState } from 'react';
 
-const Home: React.FC = () => {
-  const [productData, setProductData] = useState<oneProductType[]>([]);
+ export  async function fetchProducts()
+ {
+  let url=`${BASE_PATH_URL}/api/products`;
+  let res=await fetch(url,{cache:"no-store",});
+  // console.log(res.json);
+  if(!res.ok)
+  {
+    throw new Error('Failed to fetch');
+  }
+  return res.json();
+  // return NextResponse.json({message:"hi"})
+}
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const resp = await fetchProducts();
-        setProductData(resp);
-      } catch (error) {
-        console.error('Failed to fetch products:', error);
-      }
-    }
+export default async function Home() {
 
-    fetchData();
-  }, []);
+  let resp:Array<oneProductType>=await fetchProducts();
+ 
+// console.log(resp);
 
   return (
     <main>
